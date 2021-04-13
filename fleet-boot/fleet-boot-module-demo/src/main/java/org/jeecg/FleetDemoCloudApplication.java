a2b8dad80f3006525211335ec692c3964a18562f
wangrp <jlwrp@126.com>
2021-04-14 05:19:15 +0800

    init version

---
+++ fleet-boot/fleet-boot-module-demo/src/main/java/org/jeecg/FleetDemoCloudApplication.java
@@ -0,0 +1,14 @@
+//package org.fleet;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+//
+//@SpringBootApplication
+//@EnableFeignClients
+//public class FleetDemoCloudApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(FleetDemoCloudApplication.class, args);
+//    }
+//}
